eef535a6d1a066edd4ab127d31d5a3de47781829
Charles Ellis <hamled@hamled.name>
2016-12-08 08:34:03 -0800

    Game constructor requires player names

--- spec/game.spec.js
+++ spec/game.spec.js
@@ -5,5 +5,19 @@ describe('Game', function() {
     it('can be called', function() {
       expect(Game).toBeFunction();
     });
+
+    it('thows an Error when not given two player names', function() {
+      expect(function() {
+        return new Game();
+      }).toThrowError(Error, 'Game must be given two player names');
+
+      expect(function() {
+        return new Game('Player X');
+      }).toThrowError(Error, 'Game must be given two player names');
+
+      expect(function() {
+        return new Game('Player X', 'Player O');
+      }).not.toThrow();
+    });
   });
 });
--- src/game.js
+++ src/game.js
@@ -1,4 +1,7 @@
-const Game = function() {
+const Game = function(playerX, playerO) {
+  if(playerX === undefined || playerO === undefined) {
+    throw new Error('Game must be given two player names');
+  }
 };
 
 export default Game;
