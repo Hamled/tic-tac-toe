28fa5fc103f13ddc7b92060a4dd4ef24d2f91428
Charles Ellis <hamled@hamled.name>
2016-12-20 14:41:09 -0800

    Idiomatic Backbone 1: Indentation
    This change only updates the indentation for the contents of the Game
    model's instanceProps and staticProps objects. There are no changes to
    the non-whitespace content of any lines.

--- src/game.js
+++ src/game.js
@@ -4,161 +4,161 @@ const instanceProps = {
   urlRoot: 'http://localhost:3000/api/v1/games/'
 };
 
-instanceProps.initialize = function(options) {
-  if(!options || options.playerX === undefined || options.playerO === undefined) {
-    throw new Error('Game must be given two player names');
-  }
-
-  // Create an empty board
-  const board = new Array(Game.BOARD_POS_MAX + 1);
-  board.fill(Game.EMPTY_CELL);
-
-  // Remove the separate player name attributes
-  this.unset('playerX');
-  this.unset('playerO');
-
-  // Set all of the attributes for persistence
-  this.set({
-    players: [
-      options.playerX,
-      options.playerO
-    ],
-    outcome: this.outcome,
-    board: board,
-    played_at: new Date() // Played at is the starting date
-  });
-
-  // Tracking turns happens behind the scenes and is not an attribute
-  this.turn = 1;
-};
-
-// Instance Methods
-instanceProps.playerX = function() {
-  return this.get('players')[0];
-};
-
-instanceProps.playerO = function() {
-  return this.get('players')[1];
-};
-
-instanceProps.currentPlayer = function() {
-  return (this.turn % 2 === 0) ? this.playerO()
-                               : this.playerX();
-};
-
-instanceProps.play = function(position) {
-  if(!Game.isValidPosition(position)) {
-    throw new Error('play() requires a valid board cell position');
-  }
-
-  if(this.turn > Game.LAST_TURN || this.outcome() !== null) {
-    throw new Error('Cannot play because game is already completed');
-  }
-
-  if(this.boardAt(position) !== Game.EMPTY_CELL) {
-    throw new Error('Cannot play because that position is already marked');
-  }
-
-  this.get('board')[position] = this.playerMark(this.currentPlayer());
-  this.trigger('change:board', this);
-  this.trigger('change', this);
-
-  this.turn++;
-};
-
-instanceProps.outcome = function() {
-  var self = this;
-  var isWin = function(pos1, pos2, pos3) {
-    return self.boardAt(pos1) === self.boardAt(pos2) &&
-           self.boardAt(pos1) === self.boardAt(pos3) &&
-           self.boardAt(pos1) !== Game.EMPTY_CELL;
-  };
-
-  // Determine the winner by checking:
-  // All win conditions involving cell 0
-  if(isWin(0, 1, 2) || isWin(0, 3, 6) || isWin(0, 4, 8)) {
-    return this.boardAt(0);
-  }
-
-  // All win conditions involving cell 4
-  if(isWin(3, 4, 5) || isWin(1, 4, 7) || isWin(2, 4, 6)) {
-    return this.boardAt(4);
-  }
-
-  // All win conditions involving cell 8
-  if(isWin(6, 7, 8) || isWin(2, 5, 8)) {
-    return this.boardAt(8);
-  }
-
-  // Otherwise, no clear winner. In that case it's a tie
-  // only if the board is completely filled.
-  var outcome = Game.EMPTY_CELL; // EMPTY_CELL means tie
-  this.get('board').forEach(function(cell) {
-    if(cell === Game.EMPTY_CELL) {
-      outcome = null; // No winner yet
+  instanceProps.initialize = function(options) {
+    if(!options || options.playerX === undefined || options.playerO === undefined) {
+      throw new Error('Game must be given two player names');
     }
-  });
 
-  return outcome;
-};
+    // Create an empty board
+    const board = new Array(Game.BOARD_POS_MAX + 1);
+    board.fill(Game.EMPTY_CELL);
 
-instanceProps.boardAt = function(position) {
-  if(!Game.isValidPosition(position)) {
-    throw new Error('boardAt() requires a valid board cell position');
-  }
+    // Remove the separate player name attributes
+    this.unset('playerX');
+    this.unset('playerO');
 
-  return this.get('board')[position];
-};
+    // Set all of the attributes for persistence
+    this.set({
+      players: [
+        options.playerX,
+        options.playerO
+      ],
+      outcome: this.outcome,
+      board: board,
+      played_at: new Date() // Played at is the starting date
+    });
 
-instanceProps.playerMark = function(player) {
-  if(player === this.playerX()) {
-    return 'X';
-  } else if(player === this.playerO()) {
-    return 'O';
-  } else {
-    throw new Error(`${player} is not one of the game's players`);
-  }
-};
-
-instanceProps.boardString = function() {
-  var self = this;
-  var rowString = function(row) {
-    var r0 = self.boardAt(row * 3 + 0),
-        r1 = self.boardAt(row * 3 + 1),
-        r2 = self.boardAt(row * 3 + 2);
-
-    return ` ${r0} | ${r1} | ${r2} \n`;
+    // Tracking turns happens behind the scenes and is not an attribute
+    this.turn = 1;
   };
 
-  var dividerString = function() {
-    return '---+---+---\n';
+  // Instance Methods
+  instanceProps.playerX = function() {
+    return this.get('players')[0];
   };
 
-  return (
-    rowString(0) +
-    dividerString() +
-    rowString(1) +
-    dividerString() +
-    rowString(2));
-};
+  instanceProps.playerO = function() {
+    return this.get('players')[1];
+  };
 
-instanceProps.printBoard = function() {
-  console.log(this.boardString());
-};
+  instanceProps.currentPlayer = function() {
+    return (this.turn % 2 === 0) ? this.playerO()
+                                : this.playerX();
+  };
+
+  instanceProps.play = function(position) {
+    if(!Game.isValidPosition(position)) {
+      throw new Error('play() requires a valid board cell position');
+    }
+
+    if(this.turn > Game.LAST_TURN || this.outcome() !== null) {
+      throw new Error('Cannot play because game is already completed');
+    }
+
+    if(this.boardAt(position) !== Game.EMPTY_CELL) {
+      throw new Error('Cannot play because that position is already marked');
+    }
+
+    this.get('board')[position] = this.playerMark(this.currentPlayer());
+    this.trigger('change:board', this);
+    this.trigger('change', this);
+
+    this.turn++;
+  };
+
+  instanceProps.outcome = function() {
+    var self = this;
+    var isWin = function(pos1, pos2, pos3) {
+      return self.boardAt(pos1) === self.boardAt(pos2) &&
+            self.boardAt(pos1) === self.boardAt(pos3) &&
+            self.boardAt(pos1) !== Game.EMPTY_CELL;
+    };
+
+    // Determine the winner by checking:
+    // All win conditions involving cell 0
+    if(isWin(0, 1, 2) || isWin(0, 3, 6) || isWin(0, 4, 8)) {
+      return this.boardAt(0);
+    }
+
+    // All win conditions involving cell 4
+    if(isWin(3, 4, 5) || isWin(1, 4, 7) || isWin(2, 4, 6)) {
+      return this.boardAt(4);
+    }
+
+    // All win conditions involving cell 8
+    if(isWin(6, 7, 8) || isWin(2, 5, 8)) {
+      return this.boardAt(8);
+    }
+
+    // Otherwise, no clear winner. In that case it's a tie
+    // only if the board is completely filled.
+    var outcome = Game.EMPTY_CELL; // EMPTY_CELL means tie
+    this.get('board').forEach(function(cell) {
+      if(cell === Game.EMPTY_CELL) {
+        outcome = null; // No winner yet
+      }
+    });
+
+    return outcome;
+  };
+
+  instanceProps.boardAt = function(position) {
+    if(!Game.isValidPosition(position)) {
+      throw new Error('boardAt() requires a valid board cell position');
+    }
+
+    return this.get('board')[position];
+  };
+
+  instanceProps.playerMark = function(player) {
+    if(player === this.playerX()) {
+      return 'X';
+    } else if(player === this.playerO()) {
+      return 'O';
+    } else {
+      throw new Error(`${player} is not one of the game's players`);
+    }
+  };
+
+  instanceProps.boardString = function() {
+    var self = this;
+    var rowString = function(row) {
+      var r0 = self.boardAt(row * 3 + 0),
+          r1 = self.boardAt(row * 3 + 1),
+          r2 = self.boardAt(row * 3 + 2);
+
+      return ` ${r0} | ${r1} | ${r2} \n`;
+    };
+
+    var dividerString = function() {
+      return '---+---+---\n';
+    };
+
+    return (
+      rowString(0) +
+      dividerString() +
+      rowString(1) +
+      dividerString() +
+      rowString(2));
+  };
+
+  instanceProps.printBoard = function() {
+    console.log(this.boardString());
+  };
 
 const staticProps = {};
 
-// Static Methods
-staticProps.isValidPosition = function(position) {
-  return Number.isInteger(position) && (position >= this.BOARD_POS_MIN &&
-                                        position <= this.BOARD_POS_MAX);
-};
+  // Static Methods
+  staticProps.isValidPosition = function(position) {
+    return Number.isInteger(position) && (position >= this.BOARD_POS_MIN &&
+                                          position <= this.BOARD_POS_MAX);
+  };
 
-// Constants
-staticProps.LAST_TURN = 9;
-staticProps.BOARD_POS_MIN = 0;
-staticProps.BOARD_POS_MAX = 8;
-staticProps.EMPTY_CELL = ' ';
+  // Constants
+  staticProps.LAST_TURN = 9;
+  staticProps.BOARD_POS_MIN = 0;
+  staticProps.BOARD_POS_MAX = 8;
+  staticProps.EMPTY_CELL = ' ';
 
 // Inherit from the Base model instead of Backbone.Model
 // this allows us to use the custom overrides from Base.
