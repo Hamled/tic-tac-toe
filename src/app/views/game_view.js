38ddfbdcf28fe9fc26f113d4ec1406744d4263c0
Charles Ellis <hamled@hamled.name>
2016-12-21 12:08:29 -0800

    Board is rendered with HTML tiles

--- src/app/views/game_view.js
+++ src/app/views/game_view.js
@@ -2,12 +2,24 @@ import Backbone from 'backbone';
 
 const GameView = Backbone.View.extend({
   render: function() {
-    this.$el.html(`
-      <pre>${this.model.boardString()}</pre>
-      <input type="text" name="position">
-      <button>Play</button>`);
+    const placedTiles = this.$('.placed-tiles');
 
-    this.delegateEvents();
+    placedTiles.empty(); // Clear out any tiles we currently have
+    for(var i = 0; i < 9; i++) {
+      const row = Math.floor(i/3),
+            col = i%3,
+            val = this.model.boardAt(i);
+
+      if(val === ' ') {
+        continue;
+      }
+
+      placedTiles.append(Backbone.$(`
+        <div class="tile" data-row="r${row}" data-col="c${col}" data-val="${val}">
+          ${val}
+        </div>
+      `));
+    }
   },
 
   events: {
