4e38e4712e125d8ead8011a2a1729524e25769d7
Charles Ellis <hamled@hamled.name>
2016-12-22 14:08:13 -0800

    Pre-game application flow asks for player names
    Previously we were just launching directly into a game using hard-coded
    names. Now we're asking the user to input the names of each player
    before a game starts (including after choosing to play again).

--- src/app/views/application_view.js
+++ src/app/views/application_view.js
@@ -1,3 +1,4 @@
+import _ from 'underscore';
 import Backbone from 'backbone';
 import Game from 'app/models/game';
 import GameView from 'app/views/game_view';
@@ -7,26 +8,55 @@ const ApplicationView = Backbone.View.extend({
     this.gameView = new GameView({
       el: this.$('#board')
     });
-    this.listenTo(this.gameView, 'restart', this.newGame);
+    this.listenTo(this.gameView, 'restart', this.restart);
 
-    this.newGame();
+    this.setupTemplate = _.template(Backbone.$('#tmpl-setup-form').html());
   },
 
   render: function() {
-    this.gameView.render();
+    if(this.currentGame) {
+      // Remove the setup form
+      this.$('.setup-form').remove();
+
+      // Show the game board if we have one
+      this.$('#board').show();
+      this.gameView.render();
+    } else {
+      // Show the setup form
+      this.$el.append(this.setupTemplate());
+
+      // Make sure we're not showing the board
+      this.$('#board').hide();
+    }
   },
 
-  newGame: function() {
+  newGame: function(playerX, playerO) {
     // Create a new Game model
     this.currentGame = new Game({
-      playerX: 'Charles',
-      playerO: 'Computer'
+      playerX: playerX,
+      playerO: playerO
     });
 
     // Make the game view use it
     this.gameView.setGame(this.currentGame);
 
     this.render();
+  },
+
+  restart: function() {
+    delete this.currentGame;
+    this.render();
+  },
+
+  events: {
+    'click .start': 'onStart'
+  },
+
+  onStart: function(e) {
+    const playerX = this.$('input[name="playerX"]').val();
+    const playerO = this.$('input[name="playerO"]').val();
+
+    this.newGame(playerX, playerO);
   }
 });
 
