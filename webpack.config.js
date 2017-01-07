3906781a28858ef050b397772b3cbaef9234a7a2
Charles Ellis <hamled@hamled.name>
2017-01-06 16:10:06 -0800

    Webpack build script
    This uses the production config rather than the development config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 var webpack = require('webpack');
 var DashboardPlugin = require('webpack-dashboard/plugin');
+var merge = require('webpack-merge');
 
-module.exports = {
+var common = {
   entry: ['babel-polyfill', './src/app.js'],
   output: {
     path: './build',
@@ -13,7 +14,13 @@ module.exports = {
       exclude: /node_modules/,
       loader: 'babel-loader'
     }]
-  },
+  }
+};
+
+var production = {
+};
+
+var development = {
   devtool: 'source-map',
   devServer: {
     contentBase: './build',
@@ -29,3 +36,14 @@ module.exports = {
     new DashboardPlugin()
   ]
 };
+
+var buildTarget = process.env.npm_lifecycle_event;
+
+switch(buildTarget) {
+  case 'build':
+    module.exports = merge(common, production);
+    break;
+  default:
+    module.exports = merge(common, development);
+    break;
+}
